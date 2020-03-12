872f40d1a325c3697c15e4eb02b0bd0c12a414d9
Melanie <melanie.caro78@gmail.com>
2020-03-12 17:52:12 +0100

    fill FCSeeking Component

--- client/src/components/FCSeeking.js
+++ client/src/components/FCSeeking.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export const FCSeeking = ({ seeking }) => {
+  console.log(seeking);
   return (
     <div>
       <h2>SEEKING</h2>
+      {seeking &&
+        seeking.map(
+          ({ Icon, Name, Status }) =>
+            Status && (
+              <div>
+                <div>
+                  <img src={Icon} alt="activity icon" />
+                </div>
+                <span>{Name}</span>
+              </div>
+            )
+        )}
     </div>
   );
 };
