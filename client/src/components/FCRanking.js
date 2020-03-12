d273001111df600a028857b128fc45b185406337
Melanie <melanie.caro78@gmail.com>
2020-03-12 17:37:39 +0100

    fill FCRanking component

--- client/src/components/FCRanking.js
+++ client/src/components/FCRanking.js
@@ -4,6 +4,13 @@ export const FCRanking = ({ rank, ranking }) => {
   return (
     <div>
       <h2>RANKING</h2>
+      {ranking && (
+        <div>
+          <p>Rank: {rank}</p>
+          <p>Monthly:{ranking.Monthly}</p>
+          <p>Weekly:{ranking.Weekly}</p>
+        </div>
+      )}
     </div>
   );
 };
