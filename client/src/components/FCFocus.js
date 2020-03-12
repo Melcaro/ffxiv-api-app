36a69f0405d61f9c65a7d9e55656e057dae941fc
Melanie <melanie.caro78@gmail.com>
2020-03-12 17:48:27 +0100

    fill FCFocus Component

--- client/src/components/FCFocus.js
+++ client/src/components/FCFocus.js
@@ -4,6 +4,18 @@ export const FCFocus = ({ focus }) => {
   return (
     <div>
       <h2>FOCUS</h2>
+      {focus &&
+        focus.map(
+          ({ Icon, Name, Status }) =>
+            Status && (
+              <div>
+                <div>
+                  <img src={Icon} alt="activity icon" />
+                </div>
+                <span>{Name}</span>
+              </div>
+            )
+        )}
     </div>
   );
 };
