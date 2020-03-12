b162c58780b8533b3eaa90ddae27aaee322f702c
Melanie <melanie.caro78@gmail.com>
2020-03-12 17:26:37 +0100

    fill FCIdentity component

--- client/src/components/FCIdentity.js
+++ client/src/components/FCIdentity.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {
+  FCIdentityContainer,
+  CrestContainer,
+  ImgContainer,
+  CrestImg,
+} from '../styles/FCIdentityStyle';
+
 export const FCIdentity = ({
   active,
   activeMembers,
@@ -14,8 +21,35 @@ export const FCIdentity = ({
   estate,
 }) => {
   return (
-    <div>
+    <FCIdentityContainer>
+      <CrestContainer>
+        {crest &&
+          crest.map(image => (
+            <ImgContainer>
+              <CrestImg src={image} alt="Free Company Crest" />
+            </ImgContainer>
+          ))}
+      </CrestContainer>
       <h2>IDENTITY</h2>
-    </div>
+      <div>
+        <h3>{name}</h3>
+        <p>{slogan}</p>
+        <span>Server:{server}</span>
+        <span>DC:{dc}</span>
+        <span>Formed:{formed}</span>
+        <span>Active: {active}</span>
+        <span>Active Members:{activeMembers}</span>
+        <span>Grand Company:{grandCompany}</span>
+        <span>Tag: {tag}</span>
+        {estate && (
+          <div>
+            <span>Estate</span>
+            <span>{estate.Greeting}</span>
+            <span>{estate.Name}</span>
+            <span>{estate.Plot}</span>
+          </div>
+        )}
+      </div>
+    </FCIdentityContainer>
   );
 };
---
+++ client/src/styles/FCIdentityStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FCIdentityContainer = styled.div`
+  position: relative;
+  border: 1px solid brown;
+`;
+
+export const CrestContainer = styled.div``;
+
+export const ImgContainer = styled.div`
+  position: absolute;
+`;
+
+export const CrestImg = styled.img``;
