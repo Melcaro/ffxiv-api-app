9d471c99234605293b2028450c0b5e5bcdf6cd43
Melanie <melanie.caro78@gmail.com>
2020-03-13 14:45:37 +0100

    fill MemberIdentity component

--- client/src/components/MemberIdentity.js
+++ client/src/components/MemberIdentity.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const MemberIdentity = ({
   activeClassJob,
@@ -19,5 +20,34 @@ export const MemberIdentity = ({
   grandCompany,
   guardianDeity,
 }) => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{title}</p>
+      <p>
+        {activeClassJob} / lvl {level}
+      </p>
+      <p>
+        Class : {classID} / Job: {jobID}
+      </p>
+      <p>Birthday: {nameDay}</p>
+      <p>Guardian Deity: {guardianDeity}</p>
+      <p>{bio}</p>
+      <p>
+        Race: {race} / {tribe} {gender}
+      </p>
+      <p>
+        Free Company <span>{freeCompany}</span>
+      </p>
+      {grandCompany && (
+        <p>
+          Grand Company: {grandCompany.NameID} / {grandCompany.RankID}
+        </p>
+      )}
+      <p>Start town: {town}</p>
+      <p>
+        Server: {server} / {dC}
+      </p>
+    </div>
+  );
 };
