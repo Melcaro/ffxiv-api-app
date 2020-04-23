58ab34e5827fb9f105f22936d9087557322f9e68
Mel <melanie.caro78@gmail.com>
2020-04-23 16:48:56 +0200

    edit build

--- server/middlewares.js
+++ server/middlewares.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 function middlewares(app) {
   app.use(express.json());
-  app.use('/', express.static(__dirname + '../client/build'));
+  app.use('/', express.static(__dirname + '/../client/build'));
 }
 
 module.exports = middlewares;
